2f66defa21918a2c2f274fdeaf19d2e20d3a7fa8
uayebcaballero <uayeb.caballero@gmail.com>
2024-10-19 15:32:42 -0600

    adding cors validation

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from typing import Union
 from fastapi import FastAPI, HTTPException
 
 from utils.database import fetch_query_as_json
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True
+)
 
 @app.get("/")
 async def read_root():
